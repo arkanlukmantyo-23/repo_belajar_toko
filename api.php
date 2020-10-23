f8c15e7650c26ac36f8ab346cc380cf34ce27b8d
arkanlukmantyo-23 <63766583+arkanlukmantyo-23@users.noreply.github.com>
2020-10-23 14:13:56 +0700

    RESTful API - DELETE

--- api.php
+++ api.php
@@ -23,4 +23,19 @@ Route::get('/product', 'productController)show');
 Route::get('/product/{id}', 'productController@detail');
 Route::post('/product', 'productController@store');
 Route::put('/product/{id}','productController@update');
-Route::post('/orders', 'ordersController@store');
+Route::delete('/product/{id}', 'productController@destroy');
+
+Route::get('/product', 'productController)show');
+Route::get('/product/{id}', 'productController@detail');
+Route::post('/product', 'productController@store');
+Route::put('/product/{id}','productController@update');
+Route::delete('/product/{id}', 'productController@destroy');
+
+Route::get('/product', 'productController)show');
+Route::get('/product/{id}', 'productController@detail');
+Route::post('/product', 'productController@store');
+Route::put('/product/{id}','productController@update');
+Route::delete('/product/{id}', 'productController@destroy');
+
+}
+);
